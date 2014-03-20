3e49e86cd4b2998bea6599b08c0e2d1ff0561509
Eugene Zhuk <eugene.zhuk@gmail.com>
2014-03-19 17:44:05 -0700

    Basic layout.

--- app/src/main/java/com/ezhuk/wear/MainActivity.java
+++ app/src/main/java/com/ezhuk/wear/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
