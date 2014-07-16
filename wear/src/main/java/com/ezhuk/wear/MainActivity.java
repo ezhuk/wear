c1c79fcfca23e07db978073532a32008ba1aa25e
Eugene Zhuk <eugene.zhuk@gmail.com>
2014-07-15 22:23:32 -0700

    Added Google API client in wear project.

--- wear/src/main/java/com/ezhuk/wear/MainActivity.java
+++ wear/src/main/java/com/ezhuk/wear/MainActivity.java
@@ -10,14 +10,22 @@ import android.support.wearable.view.WatchViewStub;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.wearable.MessageApi;
 import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Wearable;
 
 import static com.ezhuk.wear.NotificationUtils.*;
 
 
-public class MainActivity extends Activity implements MessageApi.MessageListener {
+public class MainActivity extends Activity
+        implements MessageApi.MessageListener,
+                   GoogleApiClient.ConnectionCallbacks,
+                   GoogleApiClient.OnConnectionFailedListener {
     public static final String MESSAGE_PATH = "/message";
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +45,44 @@ public class MainActivity extends Activity implements MessageApi.MessageListener
                 });
             }
         });
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Wearable.API)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+ //       if (!mResolvingError) {
+            mGoogleApiClient.connect();
+ //       }
+    }
+
+    @Override
+    protected void onStop() {
+        if (null != mGoogleApiClient && mGoogleApiClient.isConnected()) {
+            Wearable.MessageApi.removeListener(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Wearable.MessageApi.addListener(mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // empty
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // empty
     }
 
     @Override
