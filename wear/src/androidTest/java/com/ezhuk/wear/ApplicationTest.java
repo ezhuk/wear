9b152854b5a766a26d86c9e3eae356d170b18d13
Eugene Zhuk <eugene.zhuk@gmail.com>
2014-08-10 18:31:15 -0700

    Added basic tests for wear.

---
+++ wear/src/androidTest/java/com/ezhuk/wear/ApplicationTest.java
@@ -0,0 +1,15 @@
+// Copyright (c) 2014 Eugene Zhuk.
+// Use of this source code is governed by the MIT license that can be found
+// in the LICENSE file.
+
+package com.ezhuk.wear;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
