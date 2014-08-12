0bc0e241fae26666b7d02f7ba335ce8bf1b70cbf
Eugene Zhuk <eugene.zhuk@gmail.com>
2014-08-11 23:18:31 -0700

    Action bar tests.

--- mobile/src/androidTest/java/com/ezhuk/wear/MainActivityTest.java
+++ mobile/src/androidTest/java/com/ezhuk/wear/MainActivityTest.java
@@ -4,6 +4,7 @@
 
 package com.ezhuk.wear;
 
+import android.app.ActionBar;
 import android.test.ActivityInstrumentationTestCase2;
 
 
@@ -22,5 +23,14 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 
     public void testPreconditions() {
         assertNotNull("mMainActivity is Null", mMainActivity);
+    }
+
+    public void testMainActivityTest_ActionBar() {
+        final ActionBar actionBar = mMainActivity.getActionBar();
+        assertNotNull("actionBar is Null", actionBar);
+
+        final int displayOptions = actionBar.getDisplayOptions();
+        assertTrue(0 == (displayOptions & ActionBar.DISPLAY_HOME_AS_UP));
+        assertTrue(0 == (displayOptions & ActionBar.DISPLAY_SHOW_HOME));
     }
 }
