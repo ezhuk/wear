43bc831e3fc921af4caf80c6d6d8399f2a20df05
Eugene Zhuk <eugene.zhuk@gmail.com>
2014-08-28 22:04:49 -0700

    Added generic data listener.

---
+++ common/src/main/java/com/ezhuk/wear/common/DataListener.java
@@ -0,0 +1,37 @@
+// Copyright (c) 2014 Eugene Zhuk.
+// Use of this source code is governed by the MIT license that can be found
+// in the LICENSE file.
+
+package com.ezhuk.wear.common;
+
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+
+import java.util.Dictionary;
+
+
+public class DataListener implements DataApi.DataListener {
+    public static interface Callback {
+        void onDataChanged(DataEvent event);
+    }
+
+    private Dictionary<String, Callback> mCallbacks;
+
+    public void addCallback(String path, Callback callback) {
+        mCallbacks.put(path, callback);
+    }
+
+    public void removeCallback(String path) {
+        mCallbacks.remove(path);
+    }
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        for (DataEvent event : dataEvents) {
+            Callback callback = mCallbacks.get(event.getDataItem().getUri().getPath());
+            if (null != callback)
+                callback.onDataChanged(event);
+        }
+    }
+}
