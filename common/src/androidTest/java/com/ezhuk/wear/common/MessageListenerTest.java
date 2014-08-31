1575e97bb909a07ae5e3b51e8ed60ae356c044e6
Eugene Zhuk <eugene.zhuk@gmail.com>
2014-08-30 17:33:46 -0700

    Added message listener unit test stub.

---
+++ common/src/androidTest/java/com/ezhuk/wear/common/MessageListenerTest.java
@@ -0,0 +1,18 @@
+// Copyright (c) 2014 Eugene Zhuk.
+// Use of this source code is governed by the MIT license that can be found
+// in the LICENSE file.
+
+package com.ezhuk.wear.common;
+
+import junit.framework.TestCase;
+
+
+public class MessageListenerTest extends TestCase {
+    protected void setUp() {
+        // empty
+    }
+
+    protected void tearDown() {
+        // empty
+    }
+}
