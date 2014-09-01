f691f0b8a5611cd675ed95ee362296fa33545fa5
Eugene Zhuk <eugene.zhuk@gmail.com>
2014-09-01 10:57:04 -0700

    Even more unit tests.

--- common/src/androidTest/java/com/ezhuk/wear/common/MessageListenerTest.java
+++ common/src/androidTest/java/com/ezhuk/wear/common/MessageListenerTest.java
@@ -12,10 +12,58 @@ import junit.framework.TestCase;
 public class MessageListenerTest extends TestCase {
     private static final String MESSAGE_PATH = "/foo";
 
+    private static class CallbackResults {
+        public int mCount;
+        public String mPath;
+        public byte[] mData;
+    }
+
     private static class MockCallback implements MessageListener.Callback {
+        private CallbackResults mResults;
+
+        public MockCallback(CallbackResults results) {
+            mResults = results;
+        }
+
         @Override
         public void onMessageReceived(MessageEvent messageEvent) {
-            // empty
+            mResults.mCount += 1;
+            mResults.mPath = messageEvent.getPath();
+            mResults.mData = messageEvent.getData();
+        }
+    }
+
+    private static class MockEvent implements MessageEvent {
+        private int mRequestId;
+        private String mNodeId;
+        private String mPath;
+        private byte[] mData;
+
+        public MockEvent(int id, String node, String path, byte[] data) {
+            mRequestId = id;
+            mNodeId = node;
+            mPath = path;
+            mData = data;
+        }
+
+        @Override
+        public int getRequestId() {
+            return mRequestId;
+        }
+
+        @Override
+        public String getSourceNodeId() {
+            return mNodeId;
+        }
+
+        @Override
+        public String getPath() {
+            return mPath;
+        }
+
+        @Override
+        public byte[] getData() {
+            return mData;
         }
     }
 
@@ -29,14 +77,26 @@ public class MessageListenerTest extends TestCase {
 
     public void testAddCallback() {
         MessageListener listener = new MessageListener();
-        listener.addCallback(MESSAGE_PATH, new MockCallback());
+        listener.addCallback(MESSAGE_PATH, new MockCallback(new CallbackResults()));
         assertNotNull(listener.getCallback(MESSAGE_PATH));
     }
 
     public void testRemoveCallback() {
         MessageListener listener = new MessageListener();
-        listener.addCallback(MESSAGE_PATH, new MockCallback());
+        listener.addCallback(MESSAGE_PATH, new MockCallback(new CallbackResults()));
         listener.removeCallback(MESSAGE_PATH);
         assertNull(listener.getCallback(MESSAGE_PATH));
+    }
+
+    public void testReceiveMessage() {
+        MessageListener listener = new MessageListener();
+        CallbackResults results = new CallbackResults();
+        listener.addCallback(MESSAGE_PATH, new MockCallback(results));
+
+        MockEvent event = new MockEvent(1, "NODE", MESSAGE_PATH, null);
+        listener.onMessageReceived(event);
+        assertEquals(1, results.mCount);
+        assertEquals(MESSAGE_PATH, results.mPath);
+        assertNull(null, results.mData);
     }
 }
